bbbcd7c7ee8ec1e4ac0a1d4c7965ede2900bb7d6
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2025-10-05 15:07:10 +0530

    refactor(nats): Stream is seperated at account level

--- captain/captain/message_broker/doctype/nats_stream/nats_stream.py
+++ captain/captain/message_broker/doctype/nats_stream/nats_stream.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, Frappe Cloud and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 from captain.message_broker.nats import NatsClient
@@ -18,17 +18,26 @@ class NATSStream(Document):
 
 		from captain.message_broker.doctype.nats_stream_subject.nats_stream_subject import NATSStreamSubject
 
+		account: DF.Data
 		stream: DF.Data
 		subjects: DF.Table[NATSStreamSubject]
 	# end: auto-generated types
 
+	def before_insert(self):
+		if not frappe.db.exists("NATS Account", {"account_name": self.account}):
+			frappe.throw(f"NATS Account {self.account} does not exist.")
+
+		# Check for stream uniqueness
+		if frappe.db.exists("NATS Stream", {"account": self.account, "stream": self.stream}):
+			frappe.throw(f"Stream {self.stream} already exists for account {self.account}.")
+
 	def on_update(self):
-		with NatsClient() as client:
+		with NatsClient(user=self.account, account=self.account) as client:
 			if self.flags.in_insert:
 				client.create_stream(self.stream, [s.subject for s in self.subjects])
 			else:
 				client.update_stream(self.stream, [s.subject for s in self.subjects])
 
 	def on_trash(self):
-		with NatsClient() as client:
+		with NatsClient(user=self.account, account=self.account) as client:
 			client.delete_stream(self.stream)
